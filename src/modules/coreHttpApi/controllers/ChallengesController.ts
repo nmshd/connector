91980dfcade9c2f8cadd0e28f51466495b07f55a
jkoenig <julian.koenig@js-soft.com>
2022-02-09 16:25:26 +0100

    feat: implement challenge routes

---
+++ src/modules/coreHttpApi/controllers/ChallengesController.ts
@@ -0,0 +1,25 @@
+import { TransportServices } from "@nmshd/runtime";
+import { Inject } from "typescript-ioc";
+import { Path, POST, Return } from "typescript-rest";
+import { Envelope } from "../../../infrastructure";
+import { BaseController } from "../common/BaseController";
+
+@Path("/api/v1/Challenges")
+export class ChallengesController extends BaseController {
+    public constructor(@Inject private readonly transportServices: TransportServices) {
+        super();
+    }
+
+    @POST
+    public async createChallenge(request: any): Promise<Return.NewResource<Envelope>> {
+        const result = await this.transportServices.challenges.createChallenge(request);
+        return this.created(result);
+    }
+
+    @POST
+    @Path("/Validate")
+    public async validateChallenge(request: any): Promise<Return.NewResource<Envelope>> {
+        const result = await this.transportServices.challenges.validateChallenge(request);
+        return this.created(result);
+    }
+}
