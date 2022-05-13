cdf8f63a47c9913d57b162da12016b16d0fc31d6
jkoenig <julian.koenig@js-soft.com>
2022-05-13 14:19:19 +0200

    chore: add samples

---
+++ .dev/mq/sampleMQReader.ts
@@ -0,0 +1,21 @@
+import rabbit from "amqplib";
+
+async function run() {
+    const connection = await rabbit.connect("amqp://localhost");
+    const channel = await connection.createChannel();
+
+    const queueName = "myQueue";
+
+    await channel.assertQueue(queueName);
+    await channel.bindQueue(queueName, "nmshd", "*.*");
+
+    await channel.consume(queueName, (msg) => {
+        if (!msg) return;
+        console.log(msg.fields);
+        console.log(msg.properties);
+        console.log(JSON.parse(msg.content.toString()));
+        channel.ack(msg);
+    });
+}
+
+run();
