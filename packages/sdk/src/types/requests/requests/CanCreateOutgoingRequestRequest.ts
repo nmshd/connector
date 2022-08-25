d48e655c01a8425b426b0cc3ba95eb4743636788
jkoenig <julian.koenig@js-soft.com>
2022-08-25 11:25:22 +0200

    refactor: make peer optional for validating outgoing requests

--- packages/sdk/src/types/requests/requests/CanCreateOutgoingRequestRequest.ts
+++ packages/sdk/src/types/requests/requests/CanCreateOutgoingRequestRequest.ts
@@ -1,3 +1,5 @@
 import { CreateOutgoingRequestRequest } from "./CreateOutgoingRequestRequest";
 
-export interface CanCreateOutgoingRequestRequest extends CreateOutgoingRequestRequest {}
+export interface CanCreateOutgoingRequestRequest extends Omit<CreateOutgoingRequestRequest, "peer"> {
+    peer?: string;
+}
