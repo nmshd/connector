3ddd9eac5b7356bcfcbc8196a1db38983b4d038b
jkoenig <julian.koenig@js-soft.com>
2023-01-04 17:34:19 +0100

    chore: uppercase variables

--- .ci/publishHelmChart.sh
+++ .ci/publishHelmChart.sh
@@ -2,9 +2,9 @@ PACKAGE_VERSION=$(jq .version -r package.json)
 
 helm package --app-version $PACKAGE_VERSION --version $PACKAGE_VERSION ./helmChart
 
-path="./connector-helm-chart-$PACKAGE_VERSION.tgz"
-registry="oci://ghcr.io/nmshd"
+PATH="./connector-helm-chart-$PACKAGE_VERSION.tgz"
+REGISTRY="oci://ghcr.io/nmshd"
 
-echo "deploying '$path' to oci registry '$registry'"
+echo "deploying '$PATH' to oci REGISTRY '$REGISTRY'"
 
-helm push $path $registry
+helm push $PATH $REGISTRY
