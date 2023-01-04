0f8bf424b4213b6112bccd72e3643242b6fc4c63
jkoenig <julian.koenig@js-soft.com>
2023-01-04 14:40:21 +0100

    ci: update chart location

--- .ci/publishHelmChart.sh
+++ .ci/publishHelmChart.sh
@@ -2,7 +2,7 @@ PACKAGE_VERSION=$(jq .version -r package.json)
 
 helm package --app-version $PACKAGE_VERSION --version $PACKAGE_VERSION ./helmChart
 
-path="./enmeshed-connector-$PACKAGE_VERSION.tgz"
+path="./connector-helm-chart-$PACKAGE_VERSION.tgz"
 registry="oci://ghcr.io/nmshd"
 
 echo "deploying '$path' to oci registry '$registry'"
