fda2a549752e14ea672db32e662a8a6f4677b3b9
jkoenig <julian.koenig@js-soft.com>
2023-01-04 14:59:01 +0100

    chore: remove audit exclude

--- .ci/runChecks.sh
+++ .ci/runChecks.sh
@@ -11,4 +11,4 @@ npm run lint:prettier
 
 # auditing
 npx license-check --ignoreRegex @nmshd/connector
-npx better-npm-audit audit --exclude 1081673,1081673
+npx better-npm-audit audit --exclude 1081673
