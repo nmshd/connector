cfdd8fde2ed563a198a355389ef554f433554dd4
jkoenig <julian.koenig@js-soft.com>
2023-03-08 14:30:45 +0100

    chore: check the environment

--- test/lib/Launcher.ts
+++ test/lib/Launcher.ts
@@ -13,6 +13,11 @@ export class Launcher {
         env["infrastructure:httpServer:port"] = port.toString();
         env["infrastructure:httpServer:apiKey"] = this.apiKey;
 
+        const notDefinedEnvironmentVariables = ["NMSHD_TEST_BASEURL", "NMSHD_TEST_CLIENTID", "NMSHD_TEST_CLIENTSECRET"].filter((env) => !process.env[env]);
+        if (notDefinedEnvironmentVariables.length > 0) {
+            throw new Error(`Missing environment variable(s): ${notDefinedEnvironmentVariables.join(", ")}}`);
+        }
+
         env["transportLibrary:baseUrl"] = process.env["NMSHD_TEST_BASEURL"];
         env["transportLibrary:platformClientId"] = process.env["NMSHD_TEST_CLIENTID"];
         env["transportLibrary:platformClientSecret"] = process.env["NMSHD_TEST_CLIENTSECRET"];
