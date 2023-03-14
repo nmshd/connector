9d744ca64ed3de64a1bb3c56da118742914d2817
Julian König <33655937+jkoenig134@users.noreply.github.com>
2023-03-14 16:45:57 +0100

    Ci/test on ferretdb (#91)
    * chore: move the development yaml file
    
    * chore: support local ferretdb testing
    
    * ci: run tests on ferretdb
    
    * chore: re-add mongo-express

--- .dev/scripts/testSetup.sh
+++ .dev/scripts/testSetup.sh
@@ -1,3 +1,3 @@
 export DATABASE_CONNECTION_STRING="mongodb://localhost:27018/?readPreference=primary&appname=connector_test&ssl=false"
 
-docker compose -f test/docker-compose.test.yml up -d mongo
+docker compose -p connector-tests -f test/docker-compose.yml up -d mongo
