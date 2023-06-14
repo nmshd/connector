9e02d5f04f917122e49bba63ab673e17d7689155
Julian König <julian.koenig@js-soft.com>
2023-06-14 16:54:27 +0200

    chore: rename the compose files

--- .dev/scripts/testSetupFerret.sh
+++ .dev/scripts/testSetupFerret.sh
@@ -1,3 +1,3 @@
 export DATABASE_CONNECTION_STRING="mongodb://localhost:27019"
 
-docker compose -p connector-tests -f test/docker-compose.yml up -d ferret
+docker compose -p connector-tests -f test/compose.yml up -d ferret
