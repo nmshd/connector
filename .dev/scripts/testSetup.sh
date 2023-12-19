66d1396891a74fe13b945113f967cb9e19b55af7
Julian König <julian.koenig@js-soft.com>
2023-12-19 11:25:08 +0100

    chore: start backbone

--- .dev/scripts/testSetup.sh
+++ .dev/scripts/testSetup.sh
@@ -1,3 +1,4 @@
 export DATABASE_CONNECTION_STRING="mongodb://localhost:27018/?readPreference=primary&appname=connector_test&ssl=false"
 
 docker compose -p connector-tests -f test/compose.yml up -d mongo
+docker compose -p connector-test-backbone -f .dev/compose.backbone.yml up -d
