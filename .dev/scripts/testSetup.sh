7b7130fab7eb05b832f1a1be51f625c13fadce6f
Julian König <julian.koenig@js-soft.com>
2023-12-19 11:26:43 +0100

    chore: set more envs

--- .dev/scripts/testSetup.sh
+++ .dev/scripts/testSetup.sh
@@ -1,4 +1,7 @@
 export DATABASE_CONNECTION_STRING="mongodb://localhost:27018/?readPreference=primary&appname=connector_test&ssl=false"
+export NMSHD_TEST_BASEURL="http://localhost:8090"
+export NMSHD_TEST_CLIENTID="test"
+export NMSHD_TEST_CLIENTSECRET="test"
 
 docker compose -p connector-tests -f test/compose.yml up -d mongo
 docker compose -p connector-test-backbone -f .dev/compose.backbone.yml up -d
