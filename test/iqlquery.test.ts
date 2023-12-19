3fb84c3a188a401258ceceec482674f16400a3a8
Julian König <julian.koenig@js-soft.com>
2023-12-19 10:26:00 +0100

    fix: test compiler errors

--- test/iqlquery.test.ts
+++ test/iqlquery.test.ts
@@ -42,7 +42,7 @@ beforeAll(async () => {
     ];
     attributeIds = [];
     for (const attribute of attributes) {
-        const attributeId = (await client1.attributes.createIdentityAttribute({ value: attribute.value, tags: attribute.tags })).result.id;
+        const attributeId = (await client1.attributes.createIdentityAttribute({ content: { value: attribute.value, tags: attribute.tags } })).result.id;
         attributeIds.push(attributeId);
     }
 
@@ -260,7 +260,7 @@ test("Remote ProposeAttributeRequest containing IQL Query without existing attri
     incomingRequestItem.attribute.owner = client1Address;
     const requestItemAttribute = incomingRequestItem.attribute;
     // TODO: also add tags in createIdentityAttribute?
-    const attributeId = (await client1.attributes.createIdentityAttribute({ value: requestItemAttribute.value })).result.id;
+    const attributeId = (await client1.attributes.createIdentityAttribute({ content: { value: requestItemAttribute.value } })).result.id;
 
     /* Reply to the response with the first matched attribute. Wait on C2 for
      * the message to arrive. */
