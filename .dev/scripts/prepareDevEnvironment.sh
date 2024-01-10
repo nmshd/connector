56e66c1301dda586380792dc4d06e4b93a514708
Julian König <julian.koenig@js-soft.com>
2024-01-10 15:14:18 +0100

    chore: be package-manage independent

--- .dev/scripts/prepareDevEnvironment.sh
+++ .dev/scripts/prepareDevEnvironment.sh
@@ -1,13 +1,19 @@
-if ! type "socat" > /dev/null; then
-  sudo apt-get install socat
+if ! type "socat" >/dev/null; then
+  if type "apt-get" >/dev/null; then
+    sudo apt-get install socat
+  fi
+
+  if type "brew" >/dev/null; then
+    brew install socat
+  fi
 fi
 
-if ! type "docker" > /dev/null; then
+if ! type "docker" >/dev/null; then
   echo "docker not found"
   exit 1
 fi
 
-if ! type "tsc" > /dev/null; then
+if ! type "tsc" >/dev/null; then
   npm i -g typescript cpx
 fi
 
