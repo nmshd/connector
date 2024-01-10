3f4b8c2cb94dc87d628f5a6238c363998d5ac8d5
Julian König <julian.koenig@js-soft.com>
2024-01-10 15:22:29 +0100

    chire: undo formatter

--- .dev/scripts/prepareDevEnvironment.sh
+++ .dev/scripts/prepareDevEnvironment.sh
@@ -1,14 +1,14 @@
-if ! type "socat" >/dev/null; then
-  if type "apt-get" >/dev/null; then
+if ! type "socat" > /dev/null; then
+  if type "apt-get" > /dev/null; then
     sudo apt-get install socat
   fi
 
-  if type "brew" >/dev/null; then
+  if type "brew" > /dev/null; then
     brew install socat
   fi
 fi
 
-if ! type "docker" >/dev/null; then
+if ! type "docker" > /dev/null; then
   echo "docker not found"
   exit 1
 fi
