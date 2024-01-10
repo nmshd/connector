57f3f0338befbb1c3d2b4c3b5e1be3d8ceead7d6
Julian König <julian.koenig@js-soft.com>
2024-01-10 14:37:14 +0100

    chore: add defaults for Launcher

--- test/lib/Launcher.ts
+++ test/lib/Launcher.ts
@@ -13,14 +13,9 @@ export class Launcher {
         env["infrastructure:httpServer:port"] = port.toString();
         env["infrastructure:httpServer:apiKey"] = this.apiKey;
 
-        const notDefinedEnvironmentVariables = ["NMSHD_TEST_BASEURL", "NMSHD_TEST_CLIENTID", "NMSHD_TEST_CLIENTSECRET"].filter((env) => !process.env[env]);
-        if (notDefinedEnvironmentVariables.length > 0) {
-            throw new Error(`Missing environment variable(s): ${notDefinedEnvironmentVariables.join(", ")}}`);
-        }
-
-        env["transportLibrary:baseUrl"] = process.env["NMSHD_TEST_BASEURL"];
-        env["transportLibrary:platformClientId"] = process.env["NMSHD_TEST_CLIENTID"];
-        env["transportLibrary:platformClientSecret"] = process.env["NMSHD_TEST_CLIENTSECRET"];
+        env["transportLibrary:baseUrl"] = process.env["NMSHD_TEST_BASEURL"] ?? "http://localhost:8090";
+        env["transportLibrary:platformClientId"] = process.env["NMSHD_TEST_CLIENTID"] ?? "test";
+        env["transportLibrary:platformClientSecret"] = process.env["NMSHD_TEST_CLIENTSECRET"] ?? "test";
 
         env.NODE_CONFIG_ENV = "test";
         env.DATABASE_NAME = accountName;
