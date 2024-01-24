e49a0d24d197fd5126cc057bf2de29b4c39d452c
Stanislaw Hüll <stanislaw.huell@js-soft.com>
2024-01-24 14:14:57 +0100

    delete obsolete request files

--- packages/sdk/src/types/attributes/requests/ShareAttributeRequest.ts
+++
@@ -1,4 +0,0 @@
-import { NotifyPeerAboutIdentityAttributeSuccessionRequest } from "./NotifyPeerAboutIdentityAttributeSuccessionRequest";
-import { ShareIdentityAttributeRequest } from "./ShareIdentityAttributeRequest";
-
-export type ShareAttributeRequest = (ShareIdentityAttributeRequest & { "@type": "Share" }) | (NotifyPeerAboutIdentityAttributeSuccessionRequest & { "@type": "Notify" });
--- packages/sdk/src/types/attributes/requests/ShareAttributeResponse.ts
+++
@@ -1,4 +0,0 @@
-import { ConnectorRequest } from "../../requests";
-import { SucceedAttributeResponse } from "./SucceedAttributeResponse";
-
-export type ShareIdentityAttributeResponse = SucceedAttributeResponse | ConnectorRequest;
