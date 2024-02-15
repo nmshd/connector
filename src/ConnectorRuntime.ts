b9980ebdb8d05bd534d84cbb9cfa4226dd698413
Sebastian Mahr <sebastian.mahr@js-soft.com>
2024-02-15 16:11:01 +0100

    chore: improve error for bad config

--- src/ConnectorRuntime.ts
+++ src/ConnectorRuntime.ts
@@ -73,7 +73,7 @@ export class ConnectorRuntime extends Runtime<ConnectorRuntimeConfig> {
         if (!result.valid) {
             let errorMessage = "The configuration is not valid:";
             for (const error of result.errors) {
-                errorMessage += `\r\n  - ${error.stack}`;
+                errorMessage += `\r\n  - ${error.stack} got ${JSON.stringify(error.instance)}`;
             }
             console.error(errorMessage); // eslint-disable-line no-console
             throw new Error(errorMessage);
