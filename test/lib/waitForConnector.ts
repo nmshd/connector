6028233b7663fdc19856e2ef053f202e2cd008ae
Sebastian Mahr <sebastian.mahr@js-soft.com>
2024-03-06 13:52:47 +0100

    chore: revert unwanted change

--- test/lib/waitForConnector.ts
+++ test/lib/waitForConnector.ts
@@ -1,11 +1,9 @@
 import { sleep } from "@js-soft/ts-utils";
 import axios from "axios";
-import inspector from "inspector";
 
 export default async function waitForConnector(port: number): Promise<void> {
     const maxRetries = 10;
-    const debug = inspector.url() !== undefined;
-    const timeout = debug ? 999999999 : 1000;
+    const timeout = 1000;
 
     const axiosInstance = axios.create({
         baseURL: `http://localhost:${port}`,
