3a28ce896d983c271fc990eb802601793c9dc2db
Julian König <julian.koenig@js-soft.com>
2024-06-26 16:56:01 +0200

    feat: restructure / resolve todos

--- src/modules/sse/SseModule.ts
+++ src/modules/sse/SseModule.ts
@@ -16,7 +16,7 @@ export interface IBackboneEventContent {
 }
 
 export default class SseModule extends ConnectorRuntimeModule {
-    private eventSource: eventSourceModule;
+    private eventSource: eventSourceModule | undefined;
 
     public constructor(runtime: ConnectorRuntime, configuration: ConnectorRuntimeModuleConfiguration, logger: ILogger, connectorMode: ConnectorMode) {
         super(runtime, configuration, logger, connectorMode);
@@ -27,6 +27,58 @@ export default class SseModule extends ConnectorRuntimeModule {
     }
 
     public async start(): Promise<void> {
+        await this.runSync();
+
+        await this.recreateEventSource();
+    }
+
+    private async recreateEventSource(): Promise<void> {
+        if (this.eventSource) {
+            try {
+                this.eventSource.close();
+            } catch (error) {
+                this.logger.error("Failed to close event source", error);
+            }
+        }
+
+        const baseURLOverride = this.connectorMode === "debug" && process.env.SSE_BASEURL_OVERRIDE ? process.env.SSE_BASEURL_OVERRIDE : null;
+        const baseUrl = baseURLOverride ?? this.runtime["runtimeConfig"].transportLibrary.baseUrl;
+        const sseUrl = `${baseUrl}/api/v1/sse`;
+
+        this.logger.info(`Connecting to SSE endpoint: ${sseUrl}`);
+
+        const token = await this.runtime.getBackboneAuthenticationToken();
+
+        const eventSource = new eventSourceModule(sseUrl, {
+            https: { rejectUnauthorized: true },
+            proxy: process.env.HTTPS_PROXY ?? process.env.HTTP_PROXY,
+            headers: { authorization: `Bearer ${token}` }
+        });
+        this.eventSource = eventSource;
+
+        eventSource.addEventListener("ExternalEventCreated", async () => await this.runSync());
+
+        await new Promise<void>((resolve, reject) => {
+            eventSource.onopen = () => {
+                this.logger.info("Connected to SSE endpoint");
+                resolve();
+
+                eventSource.onopen = () => {
+                    // noop
+                };
+            };
+
+            eventSource.onerror = (error) => {
+                reject(error);
+            };
+        });
+
+        eventSource.onerror = async (error) => {
+            if (error.status === 401) await this.recreateEventSource();
+        };
+    }
+
+    private async runSync(): Promise<void> {
         const services = this.runtime.getServices();
 
         const syncResult = await services.transportServices.account.syncEverything();
@@ -34,57 +86,9 @@ export default class SseModule extends ConnectorRuntimeModule {
             this.logger.error(syncResult);
             return;
         }
-
-        const baseUrl =
-            this.connectorMode === "debug" && process.env.SSE_BASEURL_OVERRIDE ? process.env.SSE_BASEURL_OVERRIDE : this.runtime["runtimeConfig"].transportLibrary.baseUrl;
-        const sseUrl = `${baseUrl}/api/v1/sse`;
-
-        this.logger.info(`Connecting to SSE endpoint: ${sseUrl}`);
-
-        const token = await this.runtime.getBackboneAuthenticationToken();
-
-        this.eventSource = new eventSourceModule(sseUrl, {
-            https: { rejectUnauthorized: true },
-            proxy: process.env.HTTPS_PROXY ?? process.env.HTTP_PROXY,
-            headers: { authorization: `Bearer ${token}` }
-        });
-
-        this.eventSource.addEventListener("ExternalEventCreated", async () => {
-            const syncResult = await services.transportServices.account.syncEverything();
-            if (syncResult.isError) {
-                this.logger.error(syncResult);
-                return;
-            }
-        });
-
-        await new Promise<void>((resolve, reject) => {
-            this.eventSource.onopen = () => {
-                this.logger.info("Connected to SSE endpoint");
-                resolve();
-
-                this.eventSource.onopen = () => {
-                    // noop
-                };
-            };
-
-            this.eventSource.onerror = (error) => {
-                reject(error);
-            };
-        });
-
-        this.eventSource.onerror = async (error) => {
-            if (error.status === 401) {
-                // TODO: use a better method to re-authenticate
-                // TODO: also run a sync here
-
-                this.stop();
-                await this.start();
-                return;
-            }
-        };
     }
 
     public stop(): void {
-        this.eventSource.close();
+        this.eventSource?.close();
     }
 }
