e209e0fdadfbd3a286707fd22028deee95896377
Sebastian Mahr <sebastian.mahr@js-soft.com>
2024-07-09 08:50:47 +0200

    chore: add white spaces

--- .dev/scripts/linkRuntime.sh
+++ .dev/scripts/linkRuntime.sh
@@ -7,28 +7,29 @@ cd "$CONNECTORPATH"
 
 [ -f .env ] && source "$CONNECTORPATH/.env"
 if [ -z "$RUNTIME_PATH" ]; then
-    echo "\$RUNTIME_PATH not set but required."
-    exit 1
+	echo "\$RUNTIME_PATH not set but required."
+	exit 1
 fi
 
 if ! type "entr" >/dev/null; then
-    echo "'entr' not found. Please install it via your package manager."
-    exit 1
+	echo "'entr' not found. Please install it via your package manager."
+	exit 1
 fi
 
 _copyRuntime() {
-    sleep 2
-    printf "Copying runtime ..."
-    
-    find "$RUNTIME_PATH/packages/" -maxdepth 1 -mindepth 1 -type d | while read runtimePkgPath; do
-        pkgName="$(jq -r .name "$runtimePkgPath/package.json")"
-        mkdir -p "$CONNECTORPATH/node_modules/$pkgName"
-        cp -r "$runtimePkgPath/dist" "$runtimePkgPath/src" "$CONNECTORPATH/node_modules/$pkgName"
-    done
-    echo "done!"
+	sleep 2
+	printf "Copying runtime ..."
+
+	find "$RUNTIME_PATH/packages/" -maxdepth 1 -mindepth 1 -type d | while read runtimePkgPath; do
+		pkgName="$(jq -r .name "$runtimePkgPath/package.json")"
+		mkdir -p "$CONNECTORPATH/node_modules/$pkgName"
+		cp -r "$runtimePkgPath/dist" "$runtimePkgPath/src" "$CONNECTORPATH/node_modules/$pkgName"
+	done
+	echo "done!"
 }
 
 case "$1" in
-    "") find $RUNTIME_PATH/packages/*/dist | entr -dd -r -s "$0 _copyRuntime" ;;
-    *) "$@" ;;
+	"") find $RUNTIME_PATH/packages/*/dist | entr -dd -r -s "$0 _copyRuntime" ;;
+
+	*) "$@" ;;
 esac
