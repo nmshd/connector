98755575ba6e1332170eeece78dea1fdbabcd377
Sebastian Mahr <sebastian.mahr@js-soft.com>
2024-07-19 09:43:32 +0200

    chore: update npm release

--- .ci/publishNpm.sh
+++ .ci/publishNpm.sh
@@ -2,10 +2,14 @@ set -e
 set -x
 
 if [ -z "$VERSION" ]; then
-    echo "The environment variable 'VERSION' must be set."
-    exit 1
+	echo "The environment variable 'VERSION' must be set."
+	exit 1
 fi
 
 cd packages/sdk
+npm version $VERSION
+npx enhanced-publish --if-possible --use-preid-as-tag
+
+cd packages/admin-cli
 npm version $VERSION
 npx enhanced-publish --if-possible --use-preid-as-tag
