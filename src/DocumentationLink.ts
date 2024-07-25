eb77361bd84aa1edbe864cd420f826590101911c
Julian König <julian.koenig@js-soft.com>
2024-07-25 09:34:38 +0200

    refactor: massively simplify

--- src/DocumentationLink.ts
+++ src/DocumentationLink.ts
@@ -3,18 +3,14 @@ export class DocumentationLink {
     private static readonly baseUrl: string = "https://enmeshed.eu";
 
     public static integrate__errorCodes(anchor?: string): string {
-        return this.build_errorCodes("integrate", "error-codes", anchor);
+        return this.build("integrate", "error-codes", anchor);
     }
 
-    public static operate__configuration(anchor?: string): string {
-        return this.build("operate", "configuration", anchor);
+    public static operate__configuration(anchor = ""): string {
+        return this.build("operate", "configuration", anchor.toLocaleLowerCase());
     }
 
     private static build(site: string, subSite: string, anchor = "") {
-        return `${DocumentationLink.baseUrl}/${site}/${subSite}#${anchor.toLowerCase()}`;
-    }
-
-    private static build_errorCodes(site: string, subSite: string, anchor = "") {
-        return `${DocumentationLink.baseUrl}/${site}/${subSite}#${anchor.toString()}`;
+        return `${DocumentationLink.baseUrl}/${site}/${subSite}#${anchor}`;
     }
 }
