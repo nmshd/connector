1bf6367487f0775e06a8e7ec94423c7d434831c1
Julian König <julian.koenig@js-soft.com>
2024-10-29 17:07:50 +0100

    feat: add sdk support

---
+++ packages/sdk/src/endpoints/IdentityMetadataEndpoint.ts
@@ -0,0 +1,16 @@
+import { ConnectorHttpResponse, ConnectorIdentityMetadata } from "../types";
+import { Endpoint } from "./Endpoint";
+
+export class IdentityMetadataEndpoint extends Endpoint {
+    public async upsertIdentityMetadata(request: any): Promise<ConnectorHttpResponse<ConnectorIdentityMetadata>> {
+        return await this.put("/api/v2/IdentityMetadata", request);
+    }
+
+    public async getIdentityMetadata(reference: string, key?: string): Promise<ConnectorHttpResponse<ConnectorIdentityMetadata>> {
+        return await this.get("/api/v2/IdentityMetadata", { reference: reference, key: key });
+    }
+
+    public async deleteIdentityMetadata(reference: string, key?: string): Promise<ConnectorHttpResponse<void>> {
+        return await this.delete("/api/v2/IdentityMetadata", { reference: reference, key: key });
+    }
+}
