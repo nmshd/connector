d27abb8bd85885bf0b564ec4f2a69579181cd469
Julian König <33655937+jkoenig134@users.noreply.github.com>
2025-01-31 17:05:55 +0100

    The SSE Module does not sync on reconnect (#358)
    * fix: log when actually connecting to sse
    
    * fix: sync on reconnect

--- src/modules/sse/SseModule.ts
+++ src/modules/sse/SseModule.ts
@@ -41,8 +41,6 @@ export default class SseModule extends ConnectorRuntimeModule<SseModuleConfigura
         const baseUrl = this.configuration.baseUrlOverride ?? this.runtime["runtimeConfig"].transportLibrary.baseUrl;
         const sseUrl = `${baseUrl}/api/v1/sse`;
 
-        this.logger.info(`Connecting to SSE endpoint: ${sseUrl}`);
-
         const baseOptions = { connect: { rejectUnauthorized: false } };
         const proxy = baseUrl.startsWith("https://") ? (process.env.https_proxy ?? process.env.HTTPS_PROXY) : (process.env.http_proxy ?? process.env.HTTP_PROXY);
 
@@ -50,11 +48,16 @@ export default class SseModule extends ConnectorRuntimeModule<SseModuleConfigura
             fetch: async (url, options) => {
                 const token = await this.runtime.getBackboneAuthenticationToken();
 
-                return await fetch(url, {
+                this.logger.info(`Connecting to SSE endpoint: ${sseUrl}`);
+                const response = await fetch(url, {
                     ...options,
                     dispatcher: proxy ? new ProxyAgent({ ...baseOptions, uri: proxy }) : new Agent(baseOptions),
                     headers: { ...options?.headers, authorization: `Bearer ${token}` }
                 });
+
+                this.logger.info(`Connected to SSE endpoint: ${sseUrl}`);
+
+                return response;
             }
         });
 
@@ -63,20 +66,11 @@ export default class SseModule extends ConnectorRuntimeModule<SseModuleConfigura
         eventSource.addEventListener("ExternalEventCreated", async () => await this.runSync());
 
         await new Promise<void>((resolve, reject) => {
-            eventSource.onopen = () => {
-                this.logger.info("Connected to SSE endpoint");
-                resolve();
-
-                eventSource.onopen = () => {
-                    // noop
-                };
-            };
-
-            eventSource.onerror = (error) => {
-                reject(error);
-            };
+            eventSource.onopen = () => resolve();
+            eventSource.onerror = (error) => reject(error);
         });
 
+        eventSource.onopen = async () => await this.runSync();
         eventSource.onerror = async (error) => {
             if (error.code === 401) await this.recreateEventSource();
         };
