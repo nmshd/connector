a9d09660654acd83bca41ffd85469884e8e0574e
Julian König <julian.koenig@js-soft.com>
2025-03-28 13:02:49 +0100

    fix: add interface

--- packages/types/src/AbstractConnectorRuntime.ts
+++ packages/types/src/AbstractConnectorRuntime.ts
@@ -2,7 +2,9 @@ import { IDatabaseConnection } from "@js-soft/docdb-access-abstractions";
 import { Runtime, RuntimeConfig, RuntimeServices } from "@nmshd/runtime";
 import { IConnectorInfrastructureRegistry } from "./infrastructure";
 
-export abstract class AbstractConnectorRuntime<TConfig extends RuntimeConfig = RuntimeConfig> extends Runtime<TConfig> {
+export interface ConnectorRuntimeConfig extends RuntimeConfig {
+    database: { dbName: string; dbNamePrefix: string };
+}
     public abstract override getServices(): RuntimeServices;
     public abstract getBackboneAuthenticationToken(): Promise<string>;
     public abstract readonly infrastructure: IConnectorInfrastructureRegistry;
