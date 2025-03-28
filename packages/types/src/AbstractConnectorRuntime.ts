7e9bf0a3901a44d525bad58153bf6c1fec774106
Julian König <33655937+jkoenig134@users.noreply.github.com>
2025-03-28 13:09:03 +0100

    Expose database information in connector runtime config (#405)
    * fix: add interface
    
    * fix: use interface

--- packages/types/src/AbstractConnectorRuntime.ts
+++ packages/types/src/AbstractConnectorRuntime.ts
@@ -2,7 +2,11 @@ import { IDatabaseConnection } from "@js-soft/docdb-access-abstractions";
 import { Runtime, RuntimeConfig, RuntimeServices } from "@nmshd/runtime";
 import { IConnectorInfrastructureRegistry } from "./infrastructure";
 
-export abstract class AbstractConnectorRuntime<TConfig extends RuntimeConfig = RuntimeConfig> extends Runtime<TConfig> {
+export interface ConnectorRuntimeConfig extends RuntimeConfig {
+    database: { dbName: string; dbNamePrefix: string };
+}
+
+export abstract class AbstractConnectorRuntime<TConfig extends ConnectorRuntimeConfig = ConnectorRuntimeConfig> extends Runtime<TConfig> {
     public abstract override getServices(): RuntimeServices;
     public abstract getBackboneAuthenticationToken(): Promise<string>;
     public abstract readonly infrastructure: IConnectorInfrastructureRegistry;
