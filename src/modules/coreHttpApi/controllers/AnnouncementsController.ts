3c8d63313e49ba83fb32af2b4df8c24d559bda9e
Timo Notheisen <timo.notheisen@js-soft.com>
2025-05-15 16:34:49 +0200

    refactor: use `any`

--- src/modules/coreHttpApi/controllers/AnnouncementsController.ts
+++ src/modules/coreHttpApi/controllers/AnnouncementsController.ts
@@ -1,5 +1,4 @@
 import { BaseController, Envelope } from "@nmshd/connector-types";
-import { LanguageISO639 } from "@nmshd/core-types";
 import { TransportServices } from "@nmshd/runtime";
 import { Inject } from "@nmshd/typescript-ioc";
 import { Accept, GET, Path, QueryParam } from "@nmshd/typescript-rest";
@@ -12,8 +11,8 @@ export class AnnouncementsController extends BaseController {
 
     @GET
     @Accept("application/json")
-    public async getAnnouncements(@QueryParam("language") language: LanguageISO639): Promise<Envelope> {
-        const result = await this.transportServices.announcements.getAnnouncements({ language: language });
+    public async getAnnouncements(@QueryParam("language") language: string): Promise<Envelope> {
+        const result = await this.transportServices.announcements.getAnnouncements({ language: language as any });
         return this.ok(result);
     }
 }
