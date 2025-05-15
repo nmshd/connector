7c58fe01299357998b24dd5c36d5c8f902f97226
Timo Notheisen <timo.notheisen@js-soft.com>
2025-05-15 16:31:03 +0200

    feat: use QueryParam for language

--- src/modules/coreHttpApi/controllers/AnnouncementsController.ts
+++ src/modules/coreHttpApi/controllers/AnnouncementsController.ts
@@ -1,7 +1,8 @@
 import { BaseController, Envelope } from "@nmshd/connector-types";
+import { LanguageISO639 } from "@nmshd/core-types";
 import { TransportServices } from "@nmshd/runtime";
 import { Inject } from "@nmshd/typescript-ioc";
-import { Accept, Context, GET, Path, ServiceContext } from "@nmshd/typescript-rest";
+import { Accept, GET, Path, QueryParam } from "@nmshd/typescript-rest";
 
 @Path("/api/v2/Announcements")
 export class AnnouncementsController extends BaseController {
@@ -11,8 +12,8 @@ export class AnnouncementsController extends BaseController {
 
     @GET
     @Accept("application/json")
-    public async getAnnouncements(@Context context: ServiceContext): Promise<Envelope> {
-        const result = await this.transportServices.announcements.getAnnouncements(context.request.query as any);
+    public async getAnnouncements(@QueryParam("language") language: LanguageISO639): Promise<Envelope> {
+        const result = await this.transportServices.announcements.getAnnouncements({ language: language });
         return this.ok(result);
     }
 }
