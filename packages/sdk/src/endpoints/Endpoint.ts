97056587d446680c7464f8fb1c2de0addb42f2c0
Sebastian Mahr <sebastian.mahr@js-soft.com>
2025-06-26 15:02:07 +0200

    chore: more logs in tests

--- packages/sdk/src/endpoints/Endpoint.ts
+++ packages/sdk/src/endpoints/Endpoint.ts
@@ -53,7 +53,7 @@ export abstract class Endpoint {
             const errorPayload = httpResponse.data.error;
             if (!errorPayload) {
                 throw new Error(
-                    `The http request to Connector route '${httpResponse.request.path}' failed with status '${httpResponse.status}': ${httpResponse.statusText} ${httpResponse.data}`
+                    `The http request to Connector route '${httpResponse.request.path}' failed with status '${httpResponse.status}': ${httpResponse.statusText} ${httpResponse.data} ${errorPayload}`
                 );
             }
             return ConnectorHttpResponse.error({
@@ -80,7 +80,7 @@ export abstract class Endpoint {
 
             if (!errorPayload) {
                 throw new Error(
-                    `The http request to Connector route '${httpResponse.request.path}' failed with status '${httpResponse.status}': ${httpResponse.statusText} ${httpResponse.data}`
+                    `The http request to Connector route '${httpResponse.request.path}' failed with status '${httpResponse.status}': ${httpResponse.statusText} ${httpResponse.data} ${errorPayload}`
                 );
             }
 
@@ -110,7 +110,7 @@ export abstract class Endpoint {
 
             if (!errorPayload) {
                 throw new Error(
-                    `The http request to Connector route '${httpResponse.request.path}' failed with status '${httpResponse.status}': ${httpResponse.statusText} ${httpResponse.data}`
+                    `The http request to Connector route '${httpResponse.request.path}' failed with status '${httpResponse.status}': ${httpResponse.statusText} ${httpResponse.data} ${errorPayload}`
                 );
             }
 
