9d67efbedd89b3090d77a634b6ab56e9e8c4f8df
Julian König <julian.koenig@js-soft.com>
2025-07-23 15:13:04 +0200

    fix: identity command

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,7 @@ import { hideBin } from "yargs/helpers";
 import { startConnectorCommand, yargsIdentityDeletionCancelCommand, yargsIdentityDeletionInitCommand, yargsIdentityStatusCommand } from "./cli/commands";
 
 yargs(hideBin(process.argv))
-    .command({
-        command: "identity [command]",
-        describe: "Identity related commands",
-        builder: (yargs) => {
-            return yargs.command(yargsIdentityStatusCommand);
-        },
-        handler: () => {
-            yargs.showHelp("log");
-        }
-    })
-    .command({
-        command: "identityDeletion [command]",
-        describe: "Identity deletion related commands",
-        builder: (yargs) => {
-            return yargs.command(yargsIdentityDeletionInitCommand).command(yargsIdentityDeletionCancelCommand);
-        },
-        handler: () => {
-            yargs.showHelp("log");
-        }
-    })
+    .command("identity [command]", "Identity related commands", (yargs) => yargs.command(yargsIdentityStatusCommand).demandCommand(1, "Please specify a command"))
     .command(startConnectorCommand)
     .demandCommand(1, 1, "Please specify a command")
     .scriptName("")
