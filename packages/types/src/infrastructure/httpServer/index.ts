cd3ac47182cc8c5dfb7832fb05e07745bb539702
Julian König <julian.koenig@js-soft.com>
2025-08-15 10:40:24 +0200

    chore: remove HttpServerRole

--- packages/types/src/infrastructure/httpServer/index.ts
+++ packages/types/src/infrastructure/httpServer/index.ts
@@ -2,6 +2,5 @@ export * from "./BaseController";
 export * from "./common";
 export * from "./globals";
 export * from "./HttpMethod";
-export * from "./HttpServerRole";
 export * from "./IHttpServer";
 export * from "./middlewares";
