a5702bd58d0d51dac4adb4735d098187b1f04e5a
Jakob Erben <jakobus.erben@gmail.com>
2025-10-13 10:29:21 +0200

    fix: licenses

--- .ci/runChecks.sh
+++ .ci/runChecks.sh
@@ -10,5 +10,5 @@ npm run lint:eslint
 npm run lint:prettier
 
 # auditing
-npx license-check --ignoreRegex @nmshd/connector
+npx license-check --ignoreRegex "@nmshd/connector|@sphereon/kmp-mdoc-core@0\.2\..*|credentials-context@2\..*|sjcl@1\..*"
 npx better-npm-audit audit
