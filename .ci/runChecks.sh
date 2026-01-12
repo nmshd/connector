b2a7c48d73334ada3f4405a9d07452de70dd457b
mkuhn <magnus.kuhn@js-soft.com>
2026-01-12 16:19:35 +0100

    chore: exclude vulnerability, update package-lock

--- .ci/runChecks.sh
+++ .ci/runChecks.sh
@@ -11,4 +11,4 @@ npm run lint:prettier
 
 # auditing
 npx license-check --ignoreRegex "@nmshd/.*|@sphereon/kmp-mdoc-core@0\.2\..*|credentials-context@2\..*|sjcl@1\..*"
-npx better-npm-audit audit
+npx better-npm-audit audit --exclude=1112030
