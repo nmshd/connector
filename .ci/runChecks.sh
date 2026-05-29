beeb427a245fe27220fee4fb526d63c4a61b4f71
britsta <britta.stallknecht@js-soft.com>
2026-05-29 13:04:24 +0200

    chore: changed test

--- .ci/runChecks.sh
+++ .ci/runChecks.sh
@@ -2,20 +2,18 @@ set -e
 set -x
 
 npm ci
+
+npm ls --all > before.txt # remove this
+
 npm run build:ci --workspaces --if-present
+
+npm ls --all > after.txt # remove this
+diff before.txt after.txt # remove this
 
 # linting
 npm run lint:tsc
 npm run lint:eslint
 npm run lint:prettier
-
-# remove this block
-npm ls --all > before.txt
-
-npm run build:ci --workspaces --if-present
-
-npm ls --all > after.txt
-diff before.txt after.txt
 
 # auditing
 npx license-check
