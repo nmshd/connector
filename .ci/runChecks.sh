b4d70c1eb687a011070821cba03e9d56b5ad6211
britsta <britta.stallknecht@js-soft.com>
2026-05-29 11:21:33 +0200

    chore: omit license-check exclusions

--- .ci/runChecks.sh
+++ .ci/runChecks.sh
@@ -10,5 +10,5 @@ npm run lint:eslint
 npm run lint:prettier
 
 # auditing
-npx license-check --ignoreRegex "@nmshd/.*|@sphereon/kmp-mdoc-core@0\.2\..*|credentials-context@2\..*|sjcl@1\..*"
+npx license-check --ignoreRegex
 npx better-npm-audit audit --exclude 1112030
