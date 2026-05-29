23dd4d94764a1bd9a5bce0bad1e3030c06bd6a8f
britsta <britta.stallknecht@js-soft.com>
2026-05-29 13:03:13 +0200

    chore: test

--- .ci/runChecks.sh
+++ .ci/runChecks.sh
@@ -9,6 +9,14 @@ npm run lint:tsc
 npm run lint:eslint
 npm run lint:prettier
 
+# remove this block
+npm ls --all > before.txt
+
+npm run build:ci --workspaces --if-present
+
+npm ls --all > after.txt
+diff before.txt after.txt
+
 # auditing
 npx license-check
 npx better-npm-audit audit --exclude 1112030
